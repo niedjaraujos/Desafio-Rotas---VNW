f936c1e88bd2438b282aa2bdf14928fd882cefac
niedjaraujos <naraujos1007@hotmail.com>
2025-12-12 16:10:25 -0300

    finalizado menu hamburguer

--- src/components/buttonLink/ButtonLink.jsx
+++ src/components/buttonLink/ButtonLink.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import * as S from './buttonLink.module.scss';
 
-export const ButtonLink = ({ to, children }) => {
+export const ButtonLink = ({ to, children, onClick }) => {
   return (
-    <Link to={to} className={S.link}>
+    <Link to={to} className={S.link} onClick={onClick}>
       {children}
     </Link>
   );
 };
+/* onClick repassado como props, pq um componente personalizado como esse aqui, não "adivinha" que o onClick deve funcionar.*/
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,37 @@
 import { Link } from 'react-router-dom';
 import { ButtonLink } from '../buttonLink/ButtonLink';
+import { useState } from 'react';
 
 import { Logo } from '../logo/Logo';
 import Hamburguer from '../../assets/hamburguer.png';
 import * as S from './header.module.scss';
 
 export default function Header() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
   return (
     <header className={S.header}>
       <Logo />
-      <nav>
-        <Link to="/">Home</Link>
-        <ButtonLink to="/voluntario" children="Seja Voluntario" />
+      <nav className={`${S.menu} ${menuAberto ? S.open : ''}`}>
+        <Link to="/" onClick={() => setMenuAberto(false)}>
+          Home
+        </Link>
+        <ButtonLink
+          onClick={() => setMenuAberto(false)}
+          to="/voluntario"
+          children="Seja Voluntario"
+        />
       </nav>
-      <img src={Hamburguer} alt="Menu hamburguer" className={S.hamburguer} />
+      <img
+        src={Hamburguer}
+        alt="Menu hamburguer"
+        className={S.hamburguer}
+        onClick={toggleMenu}
+      />
     </header>
   );
 }
